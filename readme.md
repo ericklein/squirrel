8e9fce715d6dfdec0aa42d1b786c99d20b26284b
sircoolio <ericklein@users.noreply.github.com>
2020-07-13 18:50:30 -0700

    logging enabled
    logging enabled and works but code is messy

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 Box that detects and annoys squirrels (or any other object)
 
 ### Purpose
-Clock template for other projects
+Protect peach trees
 
 ### Contributors
 
@@ -11,22 +11,37 @@ Clock template for other projects
 
 
 ### BOM
-
+  - 1X: Adafruit Feather M0 Proto (part 2772)
+  - 1X: Adafruit Feather MusicWing w/3A Amplifier (part 3436)
+  - 1X: Adafruit VL53L0X ToF sensor (part 3317)
 
 ### Pinouts
 
 
 ### Information Sources
-  - Overview of HC-SR04 (ultrasonic sensor) at: https://docs.google.com/document/d/1Y-yZnNhMYy7rwhAgyL_pfa39RsB-x2qR4vP8saG73rE/edit
+  - Adafruit Feather M0 Proto: https://learn.adafruit.com/adafruit-feather-m0-basic-proto
+  - HC-SR04 (ultrasonic sensor): https://docs.google.com/document/d/1Y-yZnNhMYy7rwhAgyL_pfa39RsB-x2qR4vP8saG73rE/edit
+  - Adafruit MusicWing: https://learn.adafruit.com/adafruit-music-maker-featherwing/
+  - Adafruit VL53L0X: https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout
 
 ### Issues
 
 ### Questions
 
 ### Learnings
+- 070720 - You want to validate the HC-SR04 input over multiple samples (depending on sampling rate) as I see multiple spurious signals at high sample rate
+- 070720 - As usual, check voltage requirement between sensor and MCU. For 5v HC-SR04 and 3v MCU, 10K resistor inline to MCU pin and 10K resistor on same pin to GND
+- 071120 - You can't read the battery level using A7 on Feather M0 with Music Maker due to pin 9 conflict, but you could make your own 100k-100k voltage divider between BAT and GND, and read that with a different analog pin.
 
 ### Feature Requests
+  - 070720 - Add vibration code to further annoy squirrel
+  - 070920 - Add RTC clock to solution
+  - 070920 - open a second logfile and dump DEBUG to it (via new #define DEBUG_LOGFILE)?
  
 ### Revisions
 - 070720
-    - merged code from clock.ino and feather_player.ino Adafruit example
+  - merged code from clock.ino and feather_player.ino Adafruit example
+- 070820
+  - added support for VL53L0X sensor
+- 070920
+  - added logging support to SD card already on MusicWing
--- squirrel.ino
+++ squirrel.ino
@@ -6,27 +6,33 @@
   See README.md for target information, revision history, feature requests, etc.
 */
 
+// libraries
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
+#include <TimeLib.h>
+
 // Conditionals (hardware functionality)
 #define DEBUG 			// debug messages to serial port
 //#define ULTRASONIC	// ultrasonic sensor
 
-// include SPI, MP3 and SD libraries
-#include <SPI.h>
-#include <SD.h>
-#include <Adafruit_VS1053.h>
+// for battery level read code in logging function (Adafruit Feather M0 Proto specific)
+#define VBATPIN A7
 
-// Feather M4, M0, 328, nRF52840 or 32u4
-#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
-#define VS1053_CS       6     // VS1053 chip select pin (output)
-#define VS1053_DCS     10     // VS1053 Data/command select pin (output)
-#define CARDCS          5     // Card chip select pin
-// DREQ should be an Int pin *if possible* (not possible on 32u4)
-#define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+// SD card for logging
+#define CARDCS			5     // SD Card chip select pin
+File logfile;
+
+// VS1053 for Feather M0, M4
+#define VS1053_RESET	-1	// VS1053 reset pin (not used!)
+#define VS1053_CS       6	// VS1053 chip select pin (output)
+#define VS1053_DCS		10	// VS1053 Data/command select pin (output)
+#define VS1053_DREQ     9	// VS1053 Data request, ideally an Interrupt pin
 
 Adafruit_VS1053_FilePlayer musicPlayer = 
   Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
 
-// range finding
+// distance sensor
 #ifdef ULTRASONIC
 	// HR-404SC
 	#define triggerPin	12
@@ -46,7 +52,7 @@ void setup()
 	  	{
 	    	delay(1);
 	  	}
-	 	Serial.println("Squirrel app launched");
+	 	Serial.println("Squirrel app started");
 	#endif
 
 	#ifdef ULTRASONIC
@@ -66,38 +72,69 @@ void setup()
 			#endif
 	#endif
 
+  	//pinMode(1, INPUT_PULLUP); // Adafruit suggested troubleshooting fix
 	if (! musicPlayer.begin())
 	{ // initialise the music player
     	#ifdef DEBUG
-    		Serial.println("Couldn't find VS1053, do you have the right pins defined?");
+    		Serial.println("Failed to initialize VS1503");
 		#endif
-     	while (1);
+     	while (1); // don't do anything more
   	}
 	#ifdef DEBUG
   		Serial.println("VS1053 initialized");
 	  	musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
 	#endif
 
-  
+  	// logging file
   	if (!SD.begin(CARDCS))
   	{
     	#ifdef DEBUG
-    		Serial.println(F("SD failed, or not present"));
+    		Serial.println("SD failed, or not present");
     	#endif
     	while (1);  // don't do anything more
   	}
   	#ifdef DEBUG
-  		Serial.println("SD OK!");
-		// list files
+  		Serial.println("SD card available");
+		// list files on SD card
 		printDirectory(SD.open("/"), 0);
 	#endif
-  
+
+	// create a new log file
+	char filename[] = "LOGGER00.CSV";
+	for (int i = 0; i < 100; i++)
+	{
+		filename[6] = i/10 + '0';
+		filename[7] = i%10 + '0';
+		if (! SD.exists(filename)) 
+		{
+			// only open a new file if it doesn't exist
+			logfile = SD.open(filename, FILE_WRITE); 
+			break;  // leave the loop!
+		}
+	}
+	if (! logfile)
+	{
+		#ifdef DEBUG
+			Serial.println("could not create logfile");
+		#endif
+	}
+	#ifdef DEBUG
+		Serial.print("Logging to: ");
+		Serial.println(filename);
+	#endif
+	// log file header row
+	logfile.println("datetime,battery_level,object_distance");    
+
 	// Set volume for left, right channels. lower numbers == louder volume!
 	musicPlayer.setVolume(5,5);
 
-	// If DREQ is on an interrupt pin we can do background
-	// audio playing
+	// If DREQ is on an interrupt pin we can do background audio playback
 	musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+
+	//set datetime; change before compile assuming battery is attached to preserve this
+	// replace with RTC time
+	// setTime(hr,min,sec,day,mnth,yr);
+	setTime(15,41,0,9,7,2020);
 }
 
 void loop()
@@ -131,61 +168,139 @@ int readDistance ()
 		lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
   		if (measure.RangeStatus != 4)
   		{  
-    		Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
 	  		distance = measure.RangeMilliMeter/10; // converting mm to cm
   		}
   		else
   		{
     		// phase failures have incorrect data
   			#ifdef DEBUG
-  				Serial.println("distance measurement out of range");
+  				Serial.print(measure.RangeMilliMeter);
+  				Serial.println(" ;distance measurement out of range");
 			#endif
   		}
-	#endif
-	#ifdef DEBUG
-		Serial.print("Distance to object is ");
-		Serial.print(distance);
-		Serial.println(" cm");
 	#endif
 	return (distance);
 }
 
 void annoySquirrel(int distance)
 {
-	// Start the loud music
 	if (distance < triggerDistance)
 	{
-		musicPlayer.playFullFile("/track001.mp3");
-		//musicPlayer.startPlayingFile("/track001.mp3");		
 		#ifdef DEBUG
-		Serial.println("Playing track001");
+			Serial.println("Object within range");
 		#endif
+
+	  	if (musicPlayer.readyForData())
+	  	{
+	  		//musicPlayer.sineTest(0x44, 500);
+	  		musicPlayer.playFullFile("/track001.mp3");
+
+		#ifdef DEBUG
+			Serial.println("Played music");
+		#endif
+				logEvent(distance);
+		}
+		else
+		{
+			Serial.println("Not ready for data");
+			musicPlayer.dumpRegs();
+		}
+		// Pound the fence (vibration code)
+
 	}
-	// Pound the fence
+	else
+	{
+	#ifdef DEBUG
+		Serial.println("No object in range");
+	#endif	
+	}
 }
 
-/// File listing helper
-void printDirectory(File dir, int numTabs) {
-   while(true) {
+// File listing helper
+void printDirectory(File dir, int numTabs)
+{
+   while(true)
+   {
      
      File entry =  dir.openNextFile();
-     if (! entry) {
+     if (! entry)
+     {
        // no more files
        //Serial.println("**nomorefiles**");
        break;
      }
-     for (uint8_t i=0; i<numTabs; i++) {
+     for (uint8_t i=0; i<numTabs; i++)
+     {
        Serial.print('\t');
      }
      Serial.print(entry.name());
-     if (entry.isDirectory()) {
+     if (entry.isDirectory())
+     {
        Serial.println("/");
        printDirectory(entry, numTabs+1);
-     } else {
+     }
+     else
+     {
        // files have sizes, directories do not
        Serial.print("\t\t");
        Serial.println(entry.size(), DEC);
      }
      entry.close();
    }
+}
+
+void logEvent (int distance)
+{
+	// datetime, battery level, distance
+	// datetime
+	logfile.print('"');
+	logfile.print(year());
+	logfile.print("/");
+	logfile.print(month());
+	logfile.print("/");
+	logfile.print(day());
+	logfile.print(" ");
+	logfile.print(hour());
+	logfile.print(":");
+	logfile.print(minute());
+	logfile.print(":");
+	logfile.print(second());
+	logfile.print('"');
+	logfile.print(",");
+	#ifdef DEBUG
+		Serial.print("Datetime is: ");
+		Serial.print(year());
+		Serial.print("/");
+		Serial.print(month());
+		Serial.print("/");
+		Serial.print(day());
+		Serial.print(" ");
+		Serial.print(hour());
+		Serial.print(":");
+		Serial.print(minute());
+		Serial.print(":");
+		Serial.println(second());
+	#endif
+	// battery level; This is Adafruit Feather M0 Proto specific code!
+	// float measuredvbat = analogRead(VBATPIN);
+	float measuredvbat = 0.0;
+	measuredvbat *= 2;    // we divided by 2, so multiply back
+	measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+	measuredvbat /= 1024; // convert to voltage
+	logfile.print(measuredvbat);
+	logfile.print(",");
+	#ifdef DEBUG
+		Serial.print("VBat: " ); Serial.println(measuredvbat);
+	#endif
+	// distance
+	logfile.println(distance);
+	#ifdef DEBUG
+		Serial.print("Distance to object is ");
+		Serial.print(distance);
+		Serial.println(" cm");
+	#endif
+	logfile.flush();
+	#ifdef DEBUG
+		Serial.println("log file updated");
+	#endif
 }
